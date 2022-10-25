465863a2f0833080930f9813e348615f0661ae9e
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2022-10-25 04:00:17 -0300

    .

--- backend/controllers/openingControllers.js
+++ backend/controllers/openingControllers.js
@@ -1,8 +1,10 @@
 const Opening = require('../models/openingModel')
 const mongoose = require('mongoose')
+
 const GetSingleOpening = async (req,res) => {
     const {id} = req.params
     const opening = await Opening.findById(id)
     res.status(200).json(opening)
 }
+
 module.exports = GetSingleOpening
