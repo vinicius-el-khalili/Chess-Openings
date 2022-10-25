cd0f2dfaebc5adddabc0b05e5b01e829eac9f329
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2022-10-25 06:21:36 -0300

    set react app and create board

---
+++ frontend/src/App.js
@@ -0,0 +1,100 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="board-container">
+        <div className="board">
+          <div className="row 1">
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+          </div>
+          <div className="row 2">
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+          </div>
+          <div className="row 3">
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+          </div>
+          <div className="row 4">
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+          </div>
+          <div className="row 5">
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+          </div>
+          <div className="row 6">
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+          </div>
+          <div className="row 7">
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+          </div>
+          <div className="row 8">
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+            <div className="squarew"></div>
+            <div className="squareb"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
