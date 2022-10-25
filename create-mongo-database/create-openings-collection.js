57671f015e2c20b7c5bfbe7ff1f14d5e4c98b470
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2022-10-25 02:13:03 -0300

    little fix

--- create-mongo-database/create-openings-collection.js
+++ create-mongo-database/create-openings-collection.js
@@ -30,11 +30,20 @@ const Opening = mongoose.model('Opening',openingSchema)
 // configure .env
 dotenv.config()
 
+const AddOpening = async (opening)=>{
+    await Opening.create({
+        name:opening.name,
+        eco:opening.eco,
+        fen:opening.fen,
+        moves:opening.moves
+    })
+}
+
 // connect to MongoDB and add files to collection
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
         data.map(opening=>{
-            Opening.create(opening)
+            AddOpening(opening)
         })
         console.log('done')
     })
